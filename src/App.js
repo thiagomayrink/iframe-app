55a636579e4717844160ea722f3803fb1952b515
thiagomayrink <thiagoscmpaiva@gmail.com>
2022-03-15 15:12:39 -0300

    feat: add confirm button

--- src/App.js
+++ src/App.js
@@ -2,22 +2,26 @@ import './App.css';
 
 function App() {
 
-  let closeIframe = () => {
+  let closeIframe = (action) => {
     let url =
       window.location !== window.parent.location
         ? document.referrer
         : document.location.href;
-    window.parent.postMessage({message: "close-checkout", provider: 'VTEX'}, url);
+    window.parent.postMessage({message: "close-checkout", provider: 'VTEX', action: action}, url);
   };
 
   return (
     <div className="App">
       <div className='App-body'>
-        <div className="close-btn">
+        <div className="cancel-btn">
           {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-          <a href="#" onClick={closeIframe}>Close iframe</a>
+          <a href="#" onClick={() => closeIframe('cancel_transaction')}>Cancel payment</a>
         </div>
-      </div>   
+        <div className="confirm-btn">
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a href="#" onClick={() => closeIframe('confirm_transaction')}>Confirm payment</a>
+        </div>
+      </div>
     </div>
   );
 }
